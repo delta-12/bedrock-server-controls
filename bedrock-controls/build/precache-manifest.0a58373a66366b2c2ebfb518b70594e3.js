2751a6c962f3548d8f91b06041d89be4559102af
delta-12 <btesq235@gmail.com>
2020-07-04 00:34:40 -0400

    Update production build

---
+++ bedrock-controls/build/precache-manifest.0a58373a66366b2c2ebfb518b70594e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39be8c26c1d2bc335c238b22ae090570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3482dbcbfc4297e3371d",
+    "url": "/static/css/main.02f8e503.chunk.css"
+  },
+  {
+    "revision": "f3773da52f77312e22bd",
+    "url": "/static/js/2.6988aa2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6988aa2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3482dbcbfc4297e3371d",
+    "url": "/static/js/main.c173a6aa.chunk.js"
+  },
+  {
+    "revision": "b9f2253a0330e2c262fd",
+    "url": "/static/js/runtime-main.51c13e8f.js"
+  }
+]);
--- bedrock-controls/build/service-worker.js
+++ bedrock-controls/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40352f9b583afc6577112dc8fe8dada6.js"
+  "/precache-manifest.0a58373a66366b2c2ebfb518b70594e3.js"
 );
 
 self.addEventListener('message', (event) => {
